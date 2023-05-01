86dc1ad76b415e54d392debe7c55c6c6a37c72d1
mixcs <mixcs@yandex.ru>
2023-05-01 15:35:20 -0400

    add div functions in fold01 example and comment about evaluation order

--- fold01.cpp
+++ fold01.cpp
@@ -8,9 +8,31 @@ auto sum (T ... t)
 	return res;
 }
 
+template<typename ... T>
+auto div (T ... t)
+{
+	return (t / ...);
+}
+
+template<typename ... T>
+auto div2 (T ... t)
+{
+	return (... / t);
+}
+
 int main()
 {
 	/* T is a maximum type, double in this case */
 	std::cout << sum(1,2,3.0,4,5) <<std::endl;
+	/* why 0.375? what rule is used? 0.375 = 3/2/4
+	 * looks like eval order is not guaranteed (SO answers etc)
+	 * but can't find it in c++17 standard.
+	 * P0145R3 propose solution for fixing evaluation order
+	 * (not in fold but in many contexts) */
+	std::cout << div(1.0,2.0,3.0,4.0) <<std::endl;
+	std::cout << (4.0/3.0/2.0/1.0) << std::endl;
+	/* looks like it does 1/2/3/4 but why div does not do 4/3/2/1? */
+	std::cout << div2(1.0,2.0,3.0,4.0) <<std::endl;
+	std::cout << (1.0/2.0/3.0/4.0) << std::endl;
 	return 0;
 }
