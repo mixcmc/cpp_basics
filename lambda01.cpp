1547e5d8736b5c39af2ffcf652da8c1428b89d74
mixcs <mixcs@yandex.ru>
2023-05-03 17:18:02 -0400

    [lambda01] add some magic (list of func objects)

--- lambda01.cpp
+++ lambda01.cpp
@@ -8,6 +8,34 @@ int main()
 	/* empty lambda */
 	auto f = [](){};
 	auto f1 = [](auto&& arg){ cout << "arg of type(" << typeid(arg).name() <<") = "<< arg <<endl; };
+	/* create functional objects of arbitrary types with any number of args
+	 * and pass them to other functional objects */
+	auto list = [](auto ...xs) {	
+		return [=](auto access){return access(xs...);};
+	};
+	auto head = [](auto xs) {
+		return xs([](auto first, auto ...rest) {return first;});
+	};
+	auto tail = [=](auto xs) {
+		return xs([=](auto first, auto ...rest) {return list(rest...);});
+	};
+	auto length = [](auto xs) {
+		return xs([](auto ...z){return sizeof ...(z);});
+	};
+	auto prod = [](auto xs) {
+		return xs([](auto ...args){ return (args * ...); });
+	};
+	auto l = list(1,2,3,4,5);
+	auto sum = l([](auto ...args){ return (args + ...); });
+	auto mul = l([](auto ...args){ return (args * ...); });
+	cout << "length(list(1,'2',\"3\"))=" << length(list(1,'2',"3")) << endl;
+	cout << "sum=" << sum << endl;
+	cout << "mul=" << mul << endl;
+	cout << "head(list(1,'2',\"3\"))=" << head(list(1, '2', "3")) << endl;
+	cout << "head(tail(list(1,'2',\"3\")))=" << head(tail(list(1, '2', "3"))) << endl;
+	cout << "head(tail(tail(list(1,'2',\"3\"))))=" << head(tail(tail(list(1, '2', "3")))) << endl;
+	cout << "prod(l)=" << prod(l) << endl;
+	/* head(tail(tail(tail(list(1, '2', "3"))))) => error because list need at least 1 arg */
 
 	f();
 	f1(5);
