3f27189b3bfd64044943353ceec5b3495f5e088c
mixcmc <mixcs@yandex.ru>
2023-05-04 16:31:11 +0300

    [apply01] add lambda example

--- apply01.cpp
+++ apply01.cpp
@@ -5,8 +5,11 @@ using std::endl;
 
 int mul(int a, int b) {return a*b;}
 
+auto add = [](auto a, auto b){ return a+b; };
+
 int main()
 {
-	cout << std::apply(mul, std::make_pair(2,3)) << endl;
+	cout << "mul: " << std::apply(mul, std::make_pair(2,3)) << endl;
+	cout << "add: " << std::apply(add, std::pair(1.5, 2.3)) << endl;
 	return 0;
 }
