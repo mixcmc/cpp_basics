3276ce9a5a72a935f2f4729d7dd4cae0ef80d3dd
mixcs <mixcs@yandex.ru>
2023-05-04 18:56:06 -0400

    [struct_bind01] make output prettier

--- struct_bind01.cpp
+++ struct_bind01.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <unordered_map>
 #include <string>
 
@@ -15,11 +16,11 @@ int main()
 		{"BLUE",  0x0000ff}
 	};
 	for (const auto&[name, val] : um) {
-		std::cout << name << " = " << std::hex << val << "\n";
+		std::cout << std::setfill(' ') << std::setw(5) << name << " = 0x" << std::setfill('0') << std::setw(8) <<  std::hex << val << "\n";
 	}
 
 	S s ={13, "boo"};
 	auto [a, b] = s;
-	std::cout << "a=" << a << ", b=" << b << std::endl;
+	std::cout << "a=" << std::dec << a << ", b=" << b << std::endl;
 	return 0;
 }
