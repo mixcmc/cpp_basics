c9e9928f4eea2b064b453c8b2bd86f7bb1f9cb4e
mixcmc <mixcs@yandex.ru>
2023-05-12 20:36:10 +0300

    [consteval01] add examples with const variables and constexpr functions

--- consteval01.cpp
+++ consteval01.cpp
@@ -3,9 +3,23 @@
 using std::cout, std::endl;
 
 consteval uint64_t square(int n) {return n*n;}
+constexpr int get_val(const int n){if(n > 3) return n + 1; return n;}
+
+int get_val2(void) {return 2;}
 
 int main()
 {
-	cout << square(5) << endl;
+	int n = 6;
+	const int nn = 7;
+	constexpr int nnn = 8;
+	const int n4 = get_val2();
+	cout << "5*5 = " << square(5) << endl;
+	//square(n); <-- error: n is not const
+	cout << "const int: n*n = " << square(nn) << endl;
+	cout << "constexpr int: n*n = " << square(nnn) << endl;
+	cout << "constexpr(*(const)): n*n = " << square(get_val(3)) << endl;
+	//square(get_val(n)); <-- error: the value of ‘n’ is not usable in a constant expression
+	cout << "constexpr(*(const var)): n*n = " << square(get_val(nn)) << endl;
+	//square(n4) << endl; <-- error: the value of ‘n’ is not usable in a constant expression
 	return 0;
 }
